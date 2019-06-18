dd5f22b512c619a8ca1b35f3e947e5904a4b4961
shoveria <shoveria@users.noreply.github.com>
2019-06-17 21:10:18 -0400

    First commit.

---
+++ src/app/employees/employees.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+
+@Component({
+  selector: 'app-employees',
+  templateUrl: './employees.component.html',
+  styleUrls: ['./employees.component.css']
+})
+export class EmployeesComponent implements OnInit {
+  employee: Employee = {
+    id: 1192,
+    name: 'Jim Bob'
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
