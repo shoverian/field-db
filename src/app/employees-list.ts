572963836097118bebf5b416d6f4b104382507b9
shoveria <shoveria@users.noreply.github.com>
2019-06-19 20:03:52 -0400

    Added list, selection functionality and updated  styles.

---
+++ src/app/employees-list.ts
@@ -0,0 +1,14 @@
+import { Employee } from './employee';
+
+export const EMPLOYEES: Employee[] = [
+  { id: 101, name: 'Sheila Crawford'},
+  { id: 102, name: 'Erik Wells'},
+  { id: 103, name: 'Terrence Floyd'},
+  { id: 104, name: 'Judith Gibbs'},
+  { id: 105, name: 'Marcos Weaver'},
+  { id: 106, name: 'Taylor Coleman'},
+  { id: 107, name: 'Floyd	Sharp'},
+  { id: 108, name: 'Terence	Chambers'},
+  { id: 109, name: 'Neal Cross'},
+  { id: 110, name: 'Felix	Klein'}
+]
--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../employee';
+import { EMPLOYEES } from '../employees-list';
 
 @Component({
   selector: 'app-employees',
@@ -7,9 +8,10 @@ import { Employee } from '../employee';
   styleUrls: ['./employees.component.css']
 })
 export class EmployeesComponent implements OnInit {
-  employee: Employee = {
-    id: 1192,
-    name: 'Jim Bob'
+  employees = EMPLOYEES;
+  selectedEmployee: Employee;
+  onSelect(employee: Employee): void {
+    this.selectedEmployee = employee;
   }
   constructor() { }
 
